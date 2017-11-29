d63b22a991ae1ebf2b332f7c0ef2b97bb56ab7f5
sid3u <33899536+sid3u@users.noreply.github.com>
2017-11-29 15:31:12 +0100

    Update TP.sql

--- TP.sql
+++ TP.sql
@@ -24,3 +24,26 @@ SELECT sess,intitulé, DATE_FORMAT(début,'%a %d') as jour FROM session WHERE mo
 
 Q1.7
 select nom, date_format(inscrire.date,"%Y-%m-%d") as "date d'inscription",sess, date_format(session.début,"%Y-%m-%d") as début from inscrire join session using(sess) join participant using(inscrit) WHERE inscrire.date=session.début-1
+
+Q1.8
+1.Implicite
+select distinct  E1.raison, E1.activité from entreprise E1, entreprise E2 where E1.activité=E2.activité and E1.entr!=E2.entr
+2. Explicite
+SELECT distinct E1.raison,E1.activité FROM entreprise E1 join entreprise E2 using(activité) WHERE E1.entr!=E2.entr
+
+Q2.
+
+Select round(avg(prix) as "prix moyen" from session
+
+Q2.2
+
+select sum(prix*nb_inscrits) as "montants inscriptions 1985" from session where year(début)=1985
+
+Q.2.3.
+select concat('[',min(prime),',',max(prime),']') as "fourchette" from session where principal is NOT NULL
+
+Q.3.1
+
+select durée, count(sess) as session from session group by durée       
+Q3.2
+select sess, nb_inscrit from session join  animateur using count(anim) as 'FDP'
